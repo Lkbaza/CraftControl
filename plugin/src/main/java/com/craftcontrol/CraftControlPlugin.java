7f351a35806ae56b540f3eff75aa803dd3d1ffb9
lkbaza <cocakola007228@gmail.com>
2026-07-08 23:34:59 +0700

    Refactor command handing through CraftControlCommand

--- plugin/src/main/java/com/craftcontrol/CraftControlPlugin.java
+++ plugin/src/main/java/com/craftcontrol/CraftControlPlugin.java
@@ -1,9 +1,9 @@
 package com.craftcontrol;
 
+import com.craftcontrol.command.CraftControlCommand;
 import com.craftcontrol.command.StatusCommand;
 import com.craftcontrol.service.ServerInfoService;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.craftcontrol.command.StatusCommand;
 
 public class CraftControlPlugin extends JavaPlugin {
 
@@ -16,7 +16,9 @@ public class CraftControlPlugin extends JavaPlugin {
 
         StatusCommand statusCommand = new StatusCommand(serverInfoService);
 
-        getCommand("cc").setExecutor(statusCommand);
+        CraftControlCommand craftControlCommand = new CraftControlCommand(statusCommand);
+
+        getCommand("cc").setExecutor(craftControlCommand);
 
         getLogger().info("[CraftControl] Plugin enabled!");
     }
--- plugin/src/main/java/com/craftcontrol/command/CraftControlCommand.java
+++ plugin/src/main/java/com/craftcontrol/command/CraftControlCommand.java
@@ -1,5 +1,33 @@
 package com.craftcontrol.command;
 
-public class CraftControlCommand {
-    //test
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CraftControlCommand implements CommandExecutor {
+
+
+    private final StatusCommand statusCommand;
+
+    public CraftControlCommand(StatusCommand statusCommand) {
+        this.statusCommand = statusCommand;
+    }
+
+@Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args) {
+
+        if (args.length == 0) {
+            sender.sendMessage("Использование: /cc status");
+            return true;
+        } else if (args[0].equals("status")) {
+            statusCommand.onCommand(sender, command, label, args);
+            return true;
+        } else {
+            sender.sendMessage("Неизвестная подкоманда");
+            return true;
+        }
+    }
 }
